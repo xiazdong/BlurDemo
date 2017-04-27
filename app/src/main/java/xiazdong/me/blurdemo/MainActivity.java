034976ed7ff65032896db346def288184187dac1
xiazdong <xiazdong@126.com>
2017-04-27 09:28:00 +0800

    first commit

---
+++ app/src/main/java/xiazdong/me/blurdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package xiazdong.me.blurdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
